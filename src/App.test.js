b9edd31bfb71e82f632639a503497cfc7bd2bae4
toddmath <tmatheson11186@gmail.com>
2020-01-24 22:52:20 -0800

    Add Congrats test and test-utils

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,4 @@
-import App from './App';
+import React from "react";
+import App from "./App";
 
-test('renders learn react link', () => {
-
-});
+test("renders learn react link", () => {});
--- src/components/Congrats.js
+++ src/components/Congrats.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
-export const Congrats = () => {
-  return (
-    <div>
-      Congrats
-    </div>
-  )
-}
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop isn't passed)
+ */
+export const Congrats = props => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  }
+  return <div data-test="component-congrats"></div>;
+};
