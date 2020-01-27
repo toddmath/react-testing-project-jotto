c93ec73f1472d37284cad0a52790cb1ae8706897
toddmath <tmatheson11186@gmail.com>
2020-01-26 21:21:16 -0800

    Finished coding Input component skeleton with passing tests so far

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,18 +1,37 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { Form, FormGroup, Label, Button, Input as In } from "reactstrap";
 
 class Input extends Component {
   render() {
+    const { success } = this.props;
     return (
-      <div>
-      <button />
+      <div data-test="component-input">
+        {!success && (
+          <Form inline>
+            <FormGroup className="mb-2 mx-sm-3">
+              <Label for="guessed-word" className="mr-sm-2"></Label>
+              <In
+                data-test="input-box"
+                type="search"
+                id="guessed-word"
+                name="guessed-word"
+                placeholder="enter guess"
+              />
+              <Button data-test="submit-button" type="submit" color="primary">
+                Submit
+              </Button>
+            </FormGroup>
+          </Form>
+        )}
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {};
-}
+const mapStateToProps = ({ success }) => {
+  return { success };
+};
 
 export default connect(mapStateToProps)(Input);
