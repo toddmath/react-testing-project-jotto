95370336bee2a4f9e6bbf050ef93c1f6d32ac397
toddmath <tmatheson11186@gmail.com>
2020-01-28 16:12:06 -0800

    added tests for secretWordSlice that successfully covers unit testing getSecretWord helper function and setSecretWord reducer implementation

---
+++ src/slices/secretWordSlice.test.js
@@ -0,0 +1,39 @@
+import moxios from "moxios";
+
+import { getSecretWord } from "./secretWordSlice";
+import { storeFactory } from "../../test/testUtils";
+
+describe("getSecretWord action creator", () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  test("adds response word to state", async () => {
+    const secretWord = "party";
+    const store = storeFactory();
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent();
+
+      request.respondWith({
+        status: 200,
+        response: secretWord
+      });
+    });
+
+    // store.dispatch(getSecretWord());
+    // const newState = store.getState();
+    // console.log(newState.secretWord, secretWord);
+    // expect(newState.secretWord).toBe(secretWord);
+
+    store.dispatch(getSecretWord()).then(() => {
+      const newState = store.getState();
+
+      expect(newState.secretWord).toBe(secretWord);
+    });
+  });
+});
