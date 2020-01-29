2bd5f8a83e2a707bc8ab3a54eacc94757813010d
toddmath <tmatheson11186@gmail.com>
2020-01-28 18:07:48 -0800

    connected App to redux and mapped guessedWords, secretWord and success to props. Also mapped getSecretWord to dispatch. Finally tested that all these get passed to it

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
+import { connect } from "react-redux";
+// import { createSelector } from "reselect";
 
 import { Container } from "reactstrap";
 import { Congrats, GuessedWords, Input } from "./components";
+import { getSecretWord } from "./slices/secretWordSlice";
+// import guessedWords from "./slices/guessedWordsSlice";
 
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <Container className="themed-container">
-        <h1 className="display-3">Jotto</h1>
-        <Congrats data-test="congrats-component" success={true} />
-        <Input />
-        <GuessedWords
-          data-test="guessed-component"
-          guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-        />
-      </Container>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Container className="themed-container">
+      <h1 className="display-3">Jotto</h1>
+      <Congrats data-test="congrats-component" success={true} />
+      <Input />
+      <GuessedWords
+        data-test="guessed-component"
+        guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
+      />
+    </Container>
+  );
+};
 
-export default App;
+const mapStateToProps = state => {
+  const { guessedWords, secretWord, success } = state;
+  return { guessedWords, secretWord, success };
+};
+
+// const mapStateToProps = state => ({
+//   guessedWords: state => selectGuessedWords(state),
+//   secretWord: state => selectSecretWord(state)
+// });
+
+export default connect(mapStateToProps, { getSecretWord })(App);
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,47 @@
 import React from "react";
+import { shallow } from "enzyme";
+
+import { storeFactory } from "../test/testUtils";
 import App from "./App";
 
+const setup = (initialState = {}) => {
+  return shallow(
+    <App store={storeFactory(initialState)} />,
+    initialState
+  ).dive();
+};
+
 test("renders learn react link", () => {});
+
+describe("App component", () => {
+  describe("redux props", () => {
+    test("has access to `success` state", () => {
+      const success = true;
+      const wrapper = setup({ success });
+      const successProp = wrapper.props().success;
+      // console.log(wrapper.debug());
+      expect(successProp).toBe(success);
+    });
+
+    test("has access to `secretWord` state", () => {
+      const secretWord = "party";
+      const wrapper = setup({ secretWord });
+      const secretWordProp = wrapper.props().secretWord;
+      expect(secretWordProp).toBe(secretWord);
+    });
+
+    test("has access to `guessedWords` state", () => {
+      const guessedWords = [{ guessedWord: "train", letterMatchCount: 3 }];
+      const wrapper = setup({ guessedWords });
+      const guessedWordsProp = wrapper.props().guessedWords;
+      expect(guessedWordsProp).toEqual(guessedWords);
+    });
+
+    test("`getSecretWord` action creator is a function on the props", () => {
+      const wrapper = setup();
+      // console.log(wrapper.debug());
+      const getSecretWordProp = wrapper.props().getSecretWord;
+      expect(getSecretWordProp).toBeInstanceOf(Function);
+    });
+  });
+});
